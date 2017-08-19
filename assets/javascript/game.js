4aafa192203e47e4da0acdbc91f345418bd63dff
gcdwyer <gcdwyer@gmail.com>
2017-08-19 01:19:55 -0400

    added logic-images appear on screen with rating

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,78 @@
 $(document).ready(function() {
 
-
-
-	var topics = [
-	"TBD1",
-	"TBD2",
-	"TBD3",
-	"TBD4"
-	]
+	var topics = ["cat","dog","mouse","bird"];
 
 	function displayLabels() {
 
-		var queryURL = "https://api.giphy.com/v1/gifs/search?q=basketball&api_key=2dabfa0142724e0f83be17b2f505b38d";
+		var animal = $(this).attr("data-name");
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=2dabfa0142724e0f83be17b2f505b38d";
 
-		for (var i = 0; i < topics.length; i++) {
-			console.log(topics[i]);
-
-			$(".labels").append("<button id='label' type='button'>" + topics[i] + "</button>");
-
-		}
+		// 	$(".labels").append("<button id='label' type='button'>" + topics[i] + "</button>");
 
 		$.ajax({
       		url: queryURL,
       		method: 'GET'
     	}).done(function(response) {
 
-      		console.log(response.data[i]);
+      		console.log(response);
 
-      		$(".images").html(response.data[i].images.downsized.url);
+      		for (var i = 0; i < 10; i++) {
 
+      			// RATING ======================================
 
+      			var imageDiv = $("<div class='image'>");
+      			
+      			var rating = response.data[i].rating;
+
+      			var pOne = $("<p>").text("Rating: " + rating);
+
+      			imageDiv.append(pOne);
+
+      			// IMAGE ========================================
+
+      			var imageURL = response.data[i].images.fixed_height_still.url;
+
+      			var image = $("<img>").attr("src", imageURL);
+
+      			imageDiv.append(image);
+
+      			$("#image-view").prepend(imageDiv);
+
+      			// $("#image-view").last().remove();
+
+      		}
     	});
-
 	}
 
+	// creates the buttons
+	function renderButtons() {
 
+		$("#button-view").empty();
+
+		console.log("i got here");
+
+		for (var i = 0; i < topics.length; i++) {
+			
+			var a = $("<button>");
+
+			a.addClass("image");
+
+			a.attr("data-name", topics[i]);
+
+			a.text(topics[i]);
+
+			$("#button-view").append(a);
+		}
+	}
 
 	//api.giphy.com/v1/gifs/search?q=monster&api_key=2dabfa0142724e0f83be17b2f505b38d
 
+	$(document).on("click", ".image", displayLabels);
 
+	
 
+	// displayLabels();
 
-	displayLabels();
+	renderButtons();
 
 });
